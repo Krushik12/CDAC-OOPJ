9f2464e366185b04f447cdf1029f668b8e8a3d77
krushik.bhandari_AC <krushik.bhandari.cmaug26@gmail.com>
2026-09-13 15:46:57 +0530

    Add files via upload

---
+++ Exercise /Exercise 1/ForLoop/src/program.java
@@ -0,0 +1,89 @@
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		//1 odd
+//		for(int temp =1 ; temp<=1000;temp++) {
+//			
+//			if(temp%2!=0) {
+//				
+//				System.out.print(temp + " ");
+//				
+//				
+//			}
+		//2 even
+			
+//		for(int temp =1 ; temp<=500;temp++) {
+//				
+//			if(temp%2==0) {
+//					
+//				System.out.print(temp + " ");
+//			}		
+		
+		
+		//3 print 7th number
+//		for(int temp =1 ; temp<=200;temp++) {
+//				
+//			if(temp%7==0) {
+//					
+//				System.out.print(temp + " ");
+//		}			
+//			
+			
+		//4 Pattern
+//		for(int row =1 ; row<=5;row++) {
+//			
+//			for(int col =1; col <=row;col++){
+//				
+//			System.out.print("* " );
+//		}	
+//			System.out.println("");
+//		}
+
+		//5 Pattern
+//		for(int row =1 ; row<=5;row++) {
+//			
+//			for(int col =5; col >=row;col--){
+//				
+//			System.out.print("* " );
+//		}	
+//			System.out.println("");
+//		}
+		//6 Pattern
+//		int temp=0;
+//		for(int row =1 ; row<=4;row++) {
+//			
+//			for(int col =1; col <=row;col++){
+//			temp++;
+//			System.out.print(temp +" " );
+//		}	
+//			System.out.println("");
+//		}
+
+		//7 Pattern
+//		for(int row =1 ; row<=6;row++) {
+//			
+//			for(int col =1; col <=row;col++){
+//				
+//			System.out.print( col );
+//		}	
+//			System.out.println("");
+//		}
+		//8 Pattern
+		
+		for(int row = 6 ; row>=1;row--) {
+			
+			for(int col =1; col <=row;col++){
+				
+			System.out.print( col + " " );
+		}	
+			System.out.println();
+		}
+		
+		
+		
+		
+	}
+
+}
