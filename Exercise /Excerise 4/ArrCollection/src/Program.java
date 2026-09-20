97564897bad991f6098a002c3d5eb8a1c87d2aaa
krushik.bhandari_AC <krushik.bhandari.cmaug26@gmail.com>
2026-09-20 10:40:10 +0530

    Add files via upload

---
+++ Exercise /Excerise 4/ArrCollection/src/Program.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> colors = new ArrayList<>();
+		
+		colors.add("Red");
+		colors.add("Green");
+		colors.add("Blue");
+		colors.add("Yellow");
+		
+		System.out.println(colors);
+		
+		
+		// insert at first position
+		
+		
+		colors.add(0, "Black");
+		System.out.println("Insert in FirstPosition: "+ colors);
+		
+		
+		//retrieve  an element at specified index
+		
+		
+		String color = colors.get(2);
+		System.out.println("Element at index 2 is "+ color);
+		
+		//Update
+		colors.set(1, "Orange");
+		System.out.println("Update element : "+colors);
+		
+		//remove
+		colors.remove(4);
+        System.out.println("Remove at index 4: "+colors);
+		
+		//Search
+        if (colors.contains("Blue")) {
+            System.out.println("Blue is present in the list.");
+        } else {
+            System.out.println("Blue is not present in the list.");
+        }
+        
+        if (colors.contains("Yellow")) {
+            System.out.println("Yellow is present in the list.");
+        } else {
+            System.out.println("Yellow is not present in the list.");
+        }
+        
+		
+	}
+
+}
